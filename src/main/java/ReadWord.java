9ae855ec16311d6b8af9da6e77c9385a52012364
viking <viking_yan@163.com>
2019-04-03 18:02:43 +0800

    在idea中提交代码

---
+++ src/main/java/ReadWord.java
@@ -0,0 +1,26 @@
+import org.apache.poi.POIXMLDocument;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by Viking on 2019/4/3
+ * 读取word文档中的内容
+ */
+public class ReadWord {
+    public static void main(String[] args) throws IOException {
+        String path = "F:/poi-word-test/ReadDoc.docx";
+        XWPFDocument document = new XWPFDocument(POIXMLDocument.openPackage(path));
+        List<XWPFParagraph> paragraphs = document.getParagraphs();
+        for (XWPFParagraph paragraph : paragraphs){
+            System.out.println(paragraph.getText());
+//            List<XWPFRun> runs = paragraph.getRuns();
+//            for (XWPFRun run : runs){
+//                System.out.println(run.text());
+//            }
+        }
+    }
+}
---
+++ src/main/java/ReplaceGraph.java
@@ -0,0 +1,33 @@
+import org.apache.poi.POIXMLDocument;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by yanshuai on 2019/4/3
+ * 段落替换
+ */
+public class ReplaceGraph {
+    public static void main(String[] args) throws IOException {
+        String path = "F:/poi-word-test/ReadDoc.docx";
+        XWPFDocument document = new XWPFDocument(POIXMLDocument.openPackage(path));
+        Map<String,String> map = new HashMap<String, String>();
+        List<String> list = new ArrayList<String>();
+
+    }
+    //替换段落文本
+    public void changeText(XWPFDocument document, Map<String,String> textMap){
+        //获取段落集合
+        List<XWPFParagraph> paragraphs = document.getParagraphs();
+        int i = 0;
+        for(XWPFParagraph paragraph : paragraphs) {
+            System.out.println("i--->"+i++);
+//            replaceParagraph(paragraph,textMap);
+        }
+    }
+}
